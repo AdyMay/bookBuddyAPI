d59875470a376194283bcf7b5f2ba324c07d6f46
Lindsey Adyson Huddleston <lindseyadysonhuddleston@Lindseys-iMac.attlocal.net>
2024-09-12 09:52:46 -0500

    update

--- db/client.js
+++ db/client.js
@@ -4,6 +4,8 @@ const client = new pg.Client({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = client;
